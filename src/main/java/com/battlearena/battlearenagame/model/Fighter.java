880073b2fbd22ddabd270da8f55d7754ff62ba23
Mohamed Osama <mohamedosama77793@gmail.com>
2025-12-02 14:07:42 +0200

    push team1_fighteredit

--- src/main/java/com/battlearena/battlearenagame/model/Fighter.java
+++ src/main/java/com/battlearena/battlearenagame/model/Fighter.java
@@ -1,4 +1,5 @@
 package com.battlearena.battlearenagame.model;
+
 public abstract class Fighter {
 
     protected String name;
@@ -9,40 +10,70 @@ public abstract class Fighter {
 
     //class weapon
     protected Weapon isweapon;
-    protected boolean shoot =true;
+    protected boolean willshoot = true;
 
-    public Fighter(String name,double health,double speed){
-        this.name=name;
-        this.health=health;
-        this.speed=speed;
-        this.x=0;
-        this.y=0;
+    public Fighter(String name, double health, double speed) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.x = 0;
+        this.y = 0;
     }
 
     //move of fighter
-    public void moveup(){y-=speed;}
-    public void movedown(){y+=speed;}
-    public void moveright(){x+=speed;}
-    public void moveleft(){
-        x-=speed;
+    public void moveup() {
+        y -= speed;
     }
 
-    public double shoot(){
+    public void movedown() {
+        y += speed;
+    }
+
+    public void moveright() {
+        x += speed;
+    }
+
+    public void moveleft() {
+        x -= speed;
+    }
+
+
+    public double shoot() {
+
 
     }
 
-    public void takedamage(double damage){
-        health-=damage;
-        if (health<0)
-            health=0;
-
+    public void takedamage(double damage) {
+        health -= damage;
+        if (health < 0)
+            health = 0;
     }
-    public boolean dead(){
+
+    public boolean dead() {
 
         return health == 0;
     }
 
+    //getters
+    public String getName() {
+        return name;
+    }
 
+    public double getHealth() {
+        return health;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Weapon getIsweapon() {
+        return isweapon;
+    }
 
 
 }
