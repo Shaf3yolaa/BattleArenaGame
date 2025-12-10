3b711fc3ae37ffd24d742539bad3a6669141a2e7
Eyad_Eshba <eyad.ashraf.eshba@gmail.com>
2025-12-11 00:51:13 +0200

    full movement feature added

--- src/main/java/com/battlearena/battlearenagame/physics/PhysicsEngine.java
+++ src/main/java/com/battlearena/battlearenagame/physics/PhysicsEngine.java
@@ -15,11 +15,19 @@ public class PhysicsEngine {
 
 
     public void movePlayer1(Fighter p1, boolean up, boolean down, boolean left, boolean right) {
-        double nextX = p1.getX();
-        double nextY = p1.getY();
-        double speed = p1.getSpeed();
-        double p1Width  = p1.getView().getWidth();
-        double p1Height = p1.getView().getHeight();
+        movePlayer(p1, up, down, left, right);
+    }
+
+    public void movePlayer2(Fighter p2, boolean up, boolean down, boolean left, boolean right) {
+        movePlayer(p2, up, down, left, right);
+    }
+
+    private void movePlayer(Fighter p, boolean up, boolean down, boolean left, boolean right) {
+        double nextX = p.getX();
+        double nextY = p.getY();
+        double speed = p.getSpeed();
+        double pWidth  = p.getView().getWidth();
+        double pHeight = p.getView().getHeight();
 
         if (up)
             nextY -= speed;
@@ -32,45 +40,15 @@ public class PhysicsEngine {
 
         if (nextX < 0)
             nextX = 0;
-        if (nextX > (w/2)-p1Width)
-            nextX = (w/2)-p1Width;
+        if (nextX > w-pWidth)
+            nextX = w-pWidth;
         if (nextY < 0)
             nextY = 0;
-        if (nextY > h-p1Height)
-            nextY = h-p1Height;
+        if (nextY > h-pHeight)
+            nextY = h-pHeight;
 
-        p1.setX(nextX);
-        p1.setY(nextY);
-    }
-
-    public void movePlayer2(Fighter p2, boolean up, boolean down, boolean left, boolean right) {
-        double nextX = p2.getX();
-        double nextY = p2.getY();
-        double speed = p2.getSpeed();
-        double p2Width  = p2.getView().getWidth();
-        double p2Height = p2.getView().getHeight();
-
-        if (up)
-            nextY -= speed;
-        if (down)
-            nextY += speed;
-        if (left)
-            nextX -= speed;
-        if (right)
-            nextX += speed;
-
-
-        if (nextX < w/2)
-            nextX = w/2;
-        if (nextX > w-p2Width)
-            nextX = w-p2Width;
-        if (nextY < 0)
-            nextY = 0;
-        if (nextY > h-p2Height)
-            nextY = h-p2Height;
-
-        p2.setX(nextX);
-        p2.setY(nextY);
+        p.setX(nextX);
+        p.setY(nextY);
     }
 
     public boolean checkCollision(Projectile bullet, Fighter target) {
