874182c922850cb1503c87daf5cce16b3a9b31c1
Eyad_Eshba <eyad.ashraf.eshba@gmail.com>
2025-12-11 13:54:03 +0200

    full movement feature added

--- src/main/java/com/battlearena/battlearenagame/physics/PhysicsEngine.java
+++ src/main/java/com/battlearena/battlearenagame/physics/PhysicsEngine.java
@@ -30,16 +30,7 @@ public class PhysicsEngine {
         double pHeight = p.getView().getHeight();
 
 
-        if (up) p.setRotation(270);
-        if (down) p.setRotation(90);
-        if (left) p.setRotation(180);
-        if (right) p.setRotation(0);
 
-
-        if (up && right) p.setRotation(315);
-        if (up && left) p.setRotation(225);
-        if (down && right) p.setRotation(45);
-        if (down && left) p.setRotation(135);
 
 
 
@@ -52,6 +43,18 @@ public class PhysicsEngine {
             nextX -= speed;
         if (right)
             nextX += speed;
+
+        if (up) p.setRotation(270);
+        if (down) p.setRotation(90);
+        if (left) p.setRotation(180);
+        if (right) p.setRotation(0);
+
+
+        if (up && right) p.setRotation(315);
+        if (up && left) p.setRotation(225);
+        if (down && right) p.setRotation(45);
+        if (down && left) p.setRotation(135);
+
 
         if (nextX < 0)
             nextX = 0;
